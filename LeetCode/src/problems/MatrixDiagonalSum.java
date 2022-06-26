7401d4bd38d8e409010d9b96d9883070b8dfa15e
Galip PALA <dp0035@PC-DP0035.tompay.local>
2022-06-26 17:58:00 +0300

    1572. Matrix Diagonal Sum

---
+++ LeetCode/src/problems/MatrixDiagonalSum.java
@@ -0,0 +1,22 @@
+package problems;
+
+public class MatrixDiagonalSum {
+	public static void main(String args[]) {
+//		int[][] mat = new int[][] { {1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+//		int[][] mat = new int[][] { {5} };
+		int[][] mat = new int[][] { {1,1,1,1},{1,1,1,1},{1,1,1,1},{1,1,1,1}};
+		
+		System.out.println(diagonalSum(mat));
+	}
+
+	public static int diagonalSum(int[][] mat) {
+		int rowCount = mat.length;
+		int sum = 0;
+		for(int i = 0; i < rowCount; i++) {
+			sum += mat[i][i] + mat[i][rowCount-1-i];
+		}
+		if(rowCount % 2 == 1)
+			sum = sum - mat[rowCount/2][rowCount/2];
+		return sum;
+	}
+}
