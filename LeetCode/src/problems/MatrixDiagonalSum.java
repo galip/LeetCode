5dd7ec8bee79cae59d3ddfb95980de5820eb4265
Galip PALA <dp0035@PC-DP0035.tompay.local>
2022-06-26 18:13:13 +0300

    https://leetcode.com/problems/matrix-diagonal-sum/

--- LeetCode/src/problems/MatrixDiagonalSum.java
+++ LeetCode/src/problems/MatrixDiagonalSum.java
@@ -1,5 +1,6 @@
 package problems;
 
+// https://leetcode.com/problems/matrix-diagonal-sum/
 public class MatrixDiagonalSum {
 	public static void main(String args[]) {
 //		int[][] mat = new int[][] { {1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
