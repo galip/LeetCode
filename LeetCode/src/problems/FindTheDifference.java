792d363c830d5c76ef807e27f326ab9486196be4
Galip PALA <dp0035@PC-DP0035.tompay.local>
2022-06-28 18:18:33 +0300

    389. Find the Difference

---
+++ LeetCode/src/problems/FindTheDifference.java
@@ -0,0 +1,39 @@
+package problems;
+
+// https://leetcode.com/problems/find-the-difference/
+public class FindTheDifference {
+	public static void main(String args[]) {
+		String s = "abcd";
+		String t = "abcde";
+		
+//		String s = "a";
+//		String t = "aa";
+		
+		System.out.println(findTheDifference(s, t));
+	}
+
+	public static char findTheDifference(String s, String t) {
+		char[] sCh = s.toCharArray();
+		char[] tCh = t.toCharArray();
+		
+		for(int i = 0; i < tCh.length; i++) {
+			boolean found = false;
+			for(int j = 0; j < sCh.length; j++) {
+				if(tCh[i] == sCh[j]) {
+					tCh[i] = '0';
+					sCh[j] = '0';
+					found = true;
+					j = sCh.length;
+				}
+			}
+			if(!found) {
+				return tCh[i];
+			}
+		}
+		for(int i = 0; i < tCh.length; i++) {
+			if(tCh[i] != '0')
+				return tCh[i];
+		}
+		return 0;
+	}
+}
