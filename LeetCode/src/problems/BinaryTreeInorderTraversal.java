98582748ce2a4d00364982dbad34c03dcacf505e
Galip PALA <galip.pala@tompay.com.tr>
2022-07-02 01:55:56 +0300

    94. Binary Tree Inorder Traversal

--- LeetCode/src/problems/BinaryTreeInorderTraversal.java
+++ LeetCode/src/problems/BinaryTreeInorderTraversal.java
@@ -4,26 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
+// https://leetcode.com/problems/binary-tree-inorder-traversal/
 public class BinaryTreeInorderTraversal {
 	public static void main(String args[]) {
-//		TreeNode root = new TreeNode(3);
-//		root.left = new TreeNode(1);
-//		root.right= new TreeNode(2);
-
 		TreeNode root = new TreeNode(1);
 		root.left = null;
 		root.right = new TreeNode(2);
 		root.right.left = new TreeNode(3);
-		
+
 		System.out.println(inorderTraversal(root));
-			
+
 	}
 
 	public static List<Integer> inorderTraversal(TreeNode root) {
 		List<Integer> list = new ArrayList<>();
 		if (root == null)
 			return list;
-		
+
 		Stack<TreeNode> stack = new Stack<>();
 		TreeNode currentNode = root;
 
