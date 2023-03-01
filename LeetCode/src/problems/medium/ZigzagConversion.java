6f79f34f83b8979a100127fd1801304309b3ea29
Galip PALA <galippala@gmail.com>
2023-03-02 00:59:07 +0300

    zig zag

---
+++ LeetCode/src/problems/medium/ZigzagConversion.java
@@ -0,0 +1,20 @@
+package problems.medium;
+
+//https://leetcode.com/problems/zigzag-conversion/
+public class ZigzagConversion {
+	public static void main(String args[]) {
+		String s = "PAYPALISHIRING";
+		
+		char[] c = s.toCharArray();
+		int numRows= 3;
+		char[]result = new char[c.length];
+		
+		for(Character character : c) {
+			
+		}
+	}
+	
+	public String convert(String s, int numRows) {
+		return s;
+    }
+}
