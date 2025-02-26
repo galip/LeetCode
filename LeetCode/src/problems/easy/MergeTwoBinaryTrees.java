98460a6fc5520d25e12cab5e888f2dc566f5503d
galip <galippala@gmail.com>
2025-02-26 23:04:48 +0300

    617. Merge Two Binary Trees

--- LeetCode/src/problems/easy/MergeTwoBinaryTrees.java
+++ LeetCode/src/problems/easy/MergeTwoBinaryTrees.java
@@ -5,13 +5,11 @@ public class MergeTwoBinaryTrees {
 	public static void main(String[] args) {
 		
 		// Input: root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
-		
 		TreeNode root1 = new TreeNode(1);
         root1.left = new TreeNode(3);
         root1.right = new TreeNode(2);
         root1.left.left = new TreeNode(5);
         
-        // Create root2 manually (handle 'null' correctly)
         TreeNode root2 = new TreeNode(2);
         root2.left = new TreeNode(1);
         root2.right = new TreeNode(3);
@@ -19,8 +17,6 @@ public class MergeTwoBinaryTrees {
         root2.right.right = new TreeNode(7);
         
         TreeNode resultNode = mergeTrees(root1, root2);
-        
-        printTree(resultNode);		
 	}
 	
 	public static TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
@@ -38,12 +34,5 @@ public class MergeTwoBinaryTrees {
         
         return resultNode;
     }
-	
-	public static void printTree(TreeNode node) {
-        if (node != null) {
-            printTree(node.left);  // Visit left child
-            System.out.print(node.val + " ");  // Print node value
-            printTree(node.right);  // Visit right child
-        }
-    }
+
 }
